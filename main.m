63b4bbe15ba599c5f2e0ce3ca249e6edf30c5851
Steve Moser <contact@stevemoser.org>
2015-11-02 22:58:43 -0500

    Add Xcode 1.5 changes

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DemoAssistant
+//
+//  Created by Ali Ozer on Fri Apr 26 2002.
+//  Copyright (c) 2002 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char *argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
